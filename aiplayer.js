b0c6e532c19da99d554df28ffa06946a15f5673b
Matthew Ehrlich <mattehrlich86@gmail.com>
2016-08-12 17:03:50 -0700

    Even smarter AI, blocks losing moves

--- aiplayer.js
+++ aiplayer.js
@@ -18,19 +18,28 @@ class AiPlayer {
   calculateMove(){
     let possMoves = this.possibleMoves();
     let winningPos = [];
+    let losingPos = [];
 
     possMoves.forEach(pos=>{
+      let oppMark = ( this.mark === 'X' ? 'O' : 'X');
       let clonedB = new Board(this.cloneBoard());
+      let clonedB2 = new Board(this.cloneBoard());
       clonedB.placeMark(this.mark, pos);
       if (clonedB.isWon(this.mark)) {
         winningPos.push(pos);
       }
+      clonedB2.placeMark(oppMark, pos);
+      if (clonedB2.isWon(oppMark)) {
+        losingPos.push(pos);
+      }
     });
 
-    if (winningPos.length === 0) {
-      return possMoves[Math.floor(Math.random() * possMoves.length)];
-    }else {
+    if (winningPos.length !== 0) {
       return winningPos[0];
+    } else if (losingPos.length !== 0) {
+      return losingPos[0];
+    } else {
+      return possMoves[Math.floor(Math.random() * possMoves.length)];
     }
   }
 
